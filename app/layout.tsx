857ac13eb7834e629b5734c531e7c2a428832782
dipak560035 <dipaksah2070@gmail.com>
2026-05-20 18:23:28 +0545

    Initial Gantabya EV setup

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "next-themes"
+import "./globals.css"
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
