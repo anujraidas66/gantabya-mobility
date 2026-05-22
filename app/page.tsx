18c6539de5036bd51c33686a0ccfc7e5012d3da7
sujitshahi <shahisujit9@gmail.com>
2026-05-22 19:44:03 +0545

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,20 @@ import AboutSection from "@/components/sections/core/AboutSection"
 import BlogSection from "@/components/sections/core/BlogSection"
 import TechnologySection from "@/components/sections/core/TechnologySection"
 import VehiclesSection from "@/components/sections/core/VehiclesSection"
+import Support from "./Support/page"
 
 
 export default function HomePage() {
   return (
     <main >
       
-      
+{/*       
       <VehiclesSection />
       <TechnologySection />
       <AboutSection />
-      <BlogSection />
+      <BlogSection /> */}
 
-
+      <Support />
 
 
 
