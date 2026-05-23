3902e8b09def7410eedc8ef790f77fdc7add0dc2
sujitshahi <shahisujit9@gmail.com>
2026-05-23 10:30:45 +0545

    support finished

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AboutSection from "@/components/sections/core/AboutSection"
 import BlogSection from "@/components/sections/core/BlogSection"
 import TechnologySection from "@/components/sections/core/TechnologySection"
 import VehiclesSection from "@/components/sections/core/VehiclesSection"
-import Support from "./Support/page"
+
 
 
 
@@ -10,13 +10,12 @@ export default function HomePage() {
   return (
     <main >
       
-{/*       
+      
       <VehiclesSection />
       <TechnologySection />
       <AboutSection />
-      <BlogSection /> */}
+      <BlogSection />
 
-    <Support />
     
 
 
